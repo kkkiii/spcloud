d556fb5445c7050934387e6423b5bdeb85edc51e
kikki <2624886248@qq.com>
2020-02-11 18:30:42 +0800

    a

---
+++ service-turbine-lucky/src/test/java/prof/leu/serviceturbinelucky/ServiceTurbineLuckyApplicationTests.java
@@ -0,0 +1,13 @@
+package prof.leu.serviceturbinelucky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTurbineLuckyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
