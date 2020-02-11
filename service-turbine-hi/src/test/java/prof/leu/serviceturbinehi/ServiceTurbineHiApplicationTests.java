9b7fb4b216b7dc9f4dcadb28e85a62ab866453f4
kikki <2624886248@qq.com>
2020-02-11 18:29:10 +0800

    a

---
+++ service-turbine-hi/src/test/java/prof/leu/serviceturbinehi/ServiceTurbineHiApplicationTests.java
@@ -0,0 +1,13 @@
+package prof.leu.serviceturbinehi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTurbineHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
